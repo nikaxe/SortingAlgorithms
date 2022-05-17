add9e144799532316d673fa739e5f884d91503f4
niklas axelsson <niklas.axelsson94@gmail.com>
2022-05-18 01:11:35 +0200

    updated BenchMany

--- src/BenchMany.java
+++ src/BenchMany.java
@@ -1,35 +1,34 @@
 public class BenchMany {
     public static void main(String[] args){
-        int size = 100;
-        int[] sortedSample = Bench.generateSample(size, 0, 1.0f);
-        int[] partiallySortedSample = Bench.generateSample(size, 5, 1.0f);
-        int[] randomSamplekn05 = Bench.generateSample(size, 100,0.5f);
-        int[] randomSample = Bench.generateSample(size, 100,1.0f);
-        int[] randomSamplekn10 = Bench.generateSample(size, 100,10f);
-        int[] randomSamplekn100 = Bench.generateSample(size, 100,100f);
-        int[] randomSamplekn1000 = Bench.generateSample(size, 100,1000f);
 
+        //int[] sortedSample = Bench.generateSample(size, 0, 1.0f);
+        //int[] partiallySortedSample = Bench.generateSample(size, 5, 1.0f);
+        //int[] randomSamplekn05 = Bench.generateSample(size, 100,0.5f);
+        //int[] randomSample = Bench.generateSample(size, 100,1.0f);
+        //int[] randomSamplekn10 = Bench.generateSample(size, 100,10f);
+        //int[] randomSamplekn100 = Bench.generateSample(size, 100,100f);
+        //int[] randomSamplekn1000 = Bench.generateSample(size, 100,1000f);
+        //int[] SIZE = new int[]{10, 30, 100, 300, 1000, 3000, 10000, 30000, 100000};
+        int[] SIZE = new int[]{10, 30, 100, 300, 1000, 3000, 10000, 20000, 30000, 40000, 50000, 60000, 70000, 80000, 90000, 100000};
         int numberOfTests = 10;
-        //double[] results = new double[numberOfTests];
-        //Double average = 0.0;
-        double[] average = new double[7];
-        for(int i = 0; i < numberOfTests; i++){
-            average[0] += Double.parseDouble(Bench.execute(Bench.bucketSort, sortedSample).replace(',', '.'));
-            average[1] += Double.parseDouble(Bench.execute(Bench.bucketSort, partiallySortedSample).replace(',', '.'));
-            average[2] += Double.parseDouble(Bench.execute(Bench.bucketSort, randomSamplekn05).replace(',', '.'));
-            average[3] += Double.parseDouble(Bench.execute(Bench.bucketSort, randomSample).replace(',', '.'));
-            average[4] += Double.parseDouble(Bench.execute(Bench.bucketSort, randomSamplekn10).replace(',', '.'));
-            average[5] += Double.parseDouble(Bench.execute(Bench.bucketSort, randomSamplekn100).replace(',', '.'));
-            average[6] += Double.parseDouble(Bench.execute(Bench.bucketSort, randomSamplekn1000).replace(',', '.'));
-        }
-        for(int i = 0; i < average.length; i++) {
-            average[i] /= numberOfTests;
-            System.out.printf("%6f%n",average[i]);
-        }
+        Bench.Function<int[], int[]> sortingAlgo = Bench.bucketSort;
+        double[][] average = new double[SIZE.length][7];
 
-        //String test = Bench.execute(Bench.countingSort, randomSample);
-        //test = test.replace(',', '.');
-        //System.out.println(test);
-        //System.out.println(Double.parseDouble(test));
+        for(int j = 0; j < SIZE.length; j++){
+            for(int i = 0; i < average[j].length; i++) {
+                average[j][0] += Double.parseDouble(Bench.execute(sortingAlgo, Bench.generateSample(SIZE[j], 0, 1.0f)).replace(',', '.'));
+                average[j][1] += Double.parseDouble(Bench.execute(sortingAlgo, Bench.generateSample(SIZE[j], 5, 1.0f)).replace(',', '.'));
+                average[j][2] += Double.parseDouble(Bench.execute(sortingAlgo, Bench.generateSample(SIZE[j], 100,0.5f)).replace(',', '.'));
+                average[j][3] += Double.parseDouble(Bench.execute(sortingAlgo, Bench.generateSample(SIZE[j], 100,1.0f)).replace(',', '.'));
+                average[j][4] += Double.parseDouble(Bench.execute(sortingAlgo, Bench.generateSample(SIZE[j], 100,10f)).replace(',', '.'));
+                average[j][5] += Double.parseDouble(Bench.execute(sortingAlgo, Bench.generateSample(SIZE[j], 100,100f)).replace(',', '.'));
+                average[j][6] += Double.parseDouble(Bench.execute(sortingAlgo, Bench.generateSample(SIZE[j], 100,1000f)).replace(',', '.'));
+            }
+            System.out.println("size: "+ SIZE[j]);
+            for(int i = 0; i < average[j].length; i++) {
+                average[j][i] /= numberOfTests;
+                System.out.printf("%6f%n",average[j][i]);
+            }
+        }
     }
 }
